6fdb1ba959b230921c1407ad4f665a6eb5488f9b
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 09:58:19 +1000

    Refactor provider configurations in Terraform files

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,14 +1,8 @@
-## Place your Terraform Args / Provider version args here
-/* terraform {
-  required_version = ">=1.3.0"
+terraform {
   required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "~> 3.11.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.4.3"
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "3.0.0"
     }
   }
-} */
+}
