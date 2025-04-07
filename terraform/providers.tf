96851b7b5387ef269a25df5898f7eeb01991f515
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 15:44:54 +1000

    Update OpenStack provider configuration with correct auth_url and tenant_name

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,9 @@
 provider "openstack" {
   # Configuration options
   user_name   = "admin"
-  #tenant_name = "admin"
+  tenant_name = "ibm-default"
   password    = var.openstack_password
-  auth_url    = "https://10.2.75.40"
+  auth_url    = "https://10.2.75.40icic/openstack/identity/v3"
+  domain_name = "Default"
   insecure   = true
-  #region      = "RegionOne"
 }
