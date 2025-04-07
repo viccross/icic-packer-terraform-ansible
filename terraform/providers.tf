4e1afc404a9e9d7be6575b0a58a54e80346921ec
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 15:28:06 +1000

    Refactor OpenStack provider configuration by re-adding user_name and password fields

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,8 @@
 provider "openstack" {
   # Configuration options
+  user_name   = "admin"
+  #tenant_name = "admin"
+  password    = var.openstack_password
+  #auth_url    = "http://myauthurl:5000/v3"
+  #region      = "RegionOne"
 }
