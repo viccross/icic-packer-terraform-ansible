64e90c6484410b30329bd5d3df34d7c76824e9dc
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 15:49:28 +1000

    Update OpenStack provider auth_url and ensure proper output formatting for image ID

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
 data "openstack_images_image_v2" "img" {
   name        = "zrhimg1"
-most_recent = true
+  most_recent = true
+}
+
+output "openstack_image_id" {
+  description = "The ID of the OpenStack image"
+  value       = data.openstack_images_image_v2.img.id
 }
