aa364b335e8086f500dc0b2b396ec1fefd762167
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 15:46:48 +1000

    Fix auth_url endpoint in OpenStack provider configuration

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,7 +3,7 @@ provider "openstack" {
   user_name   = "admin"
   tenant_name = "ibm-default"
   password    = var.openstack_password
-  auth_url    = "https://10.2.75.40icic/openstack/identity/v3"
+  auth_url    = "https://10.2.75.40/icic/openstack/identity/v3"
   domain_name = "Default"
   insecure   = true
 }
