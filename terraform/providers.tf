640f9e5e4fcc23677c4a8ec6199d1000b6615aec
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 15:50:09 +1000

    Update OpenStack provider user_name to 'root'

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,6 @@
 provider "openstack" {
   # Configuration options
-  user_name   = "admin"
+  user_name   = "root"
   tenant_name = "ibm-default"
   password    = var.openstack_password
   auth_url    = "https://icicmgt1.z.stg.ibm/icic/openstack/identity/v3"
