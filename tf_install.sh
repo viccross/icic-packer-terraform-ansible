b25aff8b9da5f1c6c42b9791932365cc967208b3
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 15:22:54 +1000

    Fix Terraform installation script by exporting version variable and removing unnecessary commands

--- tf_install.sh
+++ tf_install.sh
@@ -1,5 +1,4 @@
-TERRAFORM_VERSION=1.11.2
-wget --quiet "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" \
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
-mv terraform /usr/local/bin \
-rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+export TERRAFORM_VERSION=1.11.2
+wget --quiet "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+sudo mv terraform /usr/local/bin
