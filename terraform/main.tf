a8e60b00acd0b11861a7eab1896645115eb829af
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 15:25:57 +1000

    Remove commented-out Terraform resources and add OpenStack image data block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,4 @@
-## Place all your terraform resources here
-#  Locals at the top (if you are using them)
-#  Data blocks next to resources that are referencing them
-#  Reduce hard coded inputs where possible. They are used below for simplicity to show structure
-
-/* local {
-  # Local that is a map that is used for something
-  example-local {
-    key = value
-  }
+data "openstack_images_image_v2" "img" {
+  name        = "zrhimg1"
+  most_recent = true
 }
-
-data "vault_auth_backend" "kubernetes" {
-  namespace = var.namespace
-  path      = "kubernetes"
-}
-
-resource "vault_policy" "policies" {
-  namespace = var.namespace
-  name      = "name"
-  policy    = "policy"
-} */
