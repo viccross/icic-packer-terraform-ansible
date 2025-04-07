d91b4de2d4b0ed76be2546b9fb1f9a9664aa7a4b
Simon Lynch <simon.lynch@hashicorp.com>
2025-04-07 16:25:20 +1000

    Add OpenStack compute instance resource and update variable definitions

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,46 @@ data "openstack_images_image_v2" "img" {
   most_recent = true
 }
 
+#
+data "openstack_compute_flavor_v2" "size" {
+  name = "tiny"
+}
+
+data "openstack_networking_network_v2" "network" {
+  name = "StLeoLAN75"
+}
+
+
+resource "random_id" "instance_suffix" {
+  byte_length = 3
+  keepers = {
+    # Any changes to these values will generate a new ID
+    image_id  = data.openstack_images_image_v2.img.id
+    flavor_id = data.openstack_flavors_flavor_v2.small.id
+    network   = data.openstack_networking_network_v2.network.name
+    # Add other fields that should trigger a new instance when changed
+  }
+}
+
+resource "openstack_compute_instance_v2" "instance_rhl_wxk" {
+  name      = "${var.environment}-${var.project}-${var.instance_purpose}-${random_id.instance_suffix.hex}"
+  image_id  = data.openstack_images_image_v2.img.id
+  flavor_id = data.openstack_flavors_flavor_v2.small.id
+  network {
+    name = "${data.openstack_networking_network_v2.network.name}"
+  }
+}
+
+output "network_name" {
+  description = "The name of the OpenStack network"
+  value       = data.openstack_networking_network_v2.network.name
+}
+
+output "openstack_flavour_id" {
+  description = "The ID of the OpenStack image"
+  value       = data.openstack_flavors_flavor_v2.small.id
+}
+
 output "openstack_image_id" {
   description = "The ID of the OpenStack image"
   value       = data.openstack_images_image_v2.img.id
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,21 @@ variable "openstack_password" {
   description = "OpenStack password"
   type        = string
 }
+
+variable "environment" {
+  description = "Environment name (e.g. dev, test, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_purpose" {
+  description = "Purpose of the instance (e.g. web, app, db)"
+  type        = string
+  default     = "demo"
+}
+
+variable "project" {
+  description = "Project identifier"
+  type        = string
+  default     = "prj"
+}
